18fbf04a05cbea5316887e420458a04f9a241dc5
nonsonwune <nonso.chikenwune@gmail.com>
2023-01-04 10:22:08 +0100

    webhook token challenge

--- business/urls.py
+++ business/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
 path('', views.home, name='home')
+path('14b5a3b6-fd24-4132-bac3-d2460256710f', views.whatsAppWebhook, name='whatsapp-webhook')
 ]
+
+#nonsonwune.ml/14b5a3b6-fd24-4132-bac3-d2460256710f
+#Token: ba1eba9c-a425-4ab1-97a6-c7b7e61ffcca
