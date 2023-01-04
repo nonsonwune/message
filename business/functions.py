4ed8bd46b251453554265ee5d0a8efd8b26c2d3a
nonsonwune <nonso.chikenwune@gmail.com>
2023-01-04 09:37:20 +0100

    whatsapp_url, token & functions.py

---
+++ business/functions.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+import requests
+
+
+def sendWhatsAppMessage(phoneNumber, message):
+    headers = {"Authorization": settings.WHATSAPP_TOKEN}
+    payload = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phoneNumber,
+        "type": "text",
+        "text": {"body": message}
+    }
+
+    response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
+    ans = response.json()
+
+    phoneNumber = "2348136514087"
+    message = "Hello There, \n This is our first Django WhatsappMessage. \n glad to have you here. \n \n Best Regards. \n Nonso Nwune"
