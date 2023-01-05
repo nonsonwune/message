04e4ddfc3a9839a7edcf4a223c23b5db73e9718e
nonsonwune <nonso.chikenwune@gmail.com>
2023-01-05 02:10:06 +0100

    Update models.py

--- business/models.py
+++ business/models.py
@@ -58,5 +58,37 @@ class BusinessPlan(models.Model):
             super(Profile, self).save(*args, **kwargs)
 
 
+def chatSession(models.Model):
+    
+    OPTIONS = [
+        ('1', 'Private Limited Company (LTD)'),
+        ('2', 'Public Limited Company (PLC)'),
+        ('3', 'Non-Profit NGO'),
+        ('4', 'Partnership')
+    ]
+    
+    business_name = models.TextField(null=True, blank=True)
+    business_type = models.CharField(choices=OPTIONS, null=True, blank=True, max_length=200)
+    country = models.TextField(null=True, blank=True)
+    prod_serv = models.TextField(null=True, blank=True)
+    short_description = models.TextField(null=True, blank=True)
+    years = models.IntegerField(null=True, blank=True)
+    progress = models.TextField(null=True, blank=True)
+
+    #util
+    uniqueId = models.CharField(null=True, blank=True, unique=True, max_length=100)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+
+            self.last_updated = timezone.localtime(timezone.now())
+            super(Profile, self).save(*args, **kwargs)
+
+
 # class FinancialModel(models.Model):
 #     business_plan = models.OneToOneField(BusinessPlan, null=True, blank=True, on_delete=models.CASCADE)
